c8e19a8a8867f7d04bb4d630f5f1e97559ae15ad
umutulupinar <umutulupinarrr@gmail.com>
2023-08-09 20:51:37 +0300

    refactor Id property and 2nd migration

--- BUSINESS/User.Core/Entities/BaseEntity.cs
+++ BUSINESS/User.Core/Entities/BaseEntity.cs
@@ -1,9 +1,11 @@
-﻿using User.Core.Enums;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using User.Core.Enums;
 
 namespace User.Core.Entities;
 
 public class BaseEntity
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id{ get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime UpdatedTime { get; set; }
