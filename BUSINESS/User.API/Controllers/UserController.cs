3be7bf7aac2a08b5fb9ad062eaf03d7ff23c55c2
umutulupinar <umutulupinarrr@gmail.com>
2023-08-09 20:30:42 +0300

    user register, activation and login actions are coded

---
+++ BUSINESS/User.API/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using User.API.Models.User;
+using User.Service.User;
+
+namespace User.API.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class UserController : Controller
+{
+
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    /// <summary>
+    /// this action provides to register user as passive
+    /// </summary>
+    /// <param name="requestModel"></param>
+    [HttpPost]
+    public async Task Register(UserRegisterRequestModel requestModel)
+        => await _userService.Register(requestModel);
+
+    /// <summary>
+    /// this action provides to activate user by changing user status to active
+    /// </summary>
+    [HttpPost]
+    public async Task ActivateAccount()
+        => await _userService.ActivateAccount();
+
+    /// <summary>
+    /// this action provides to login by UserName-password or Mail-Password
+    /// </summary>
+    /// <param name="requestModel"></param>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<UserLoginResponseModel> Login(UserLoginRequestModel requestModel)
+        => await _userService.Login(requestModel);
+}
