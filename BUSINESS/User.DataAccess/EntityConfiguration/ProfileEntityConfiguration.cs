92cc153d7b021e90c79cc652f8c1cfcf130e5781
umutulupinar <umutulupinarrr@gmail.com>
2023-08-09 18:51:40 +0300

    Entity configs. are coded

--- BUSINESS/User.DataAccess/EntityConfiguration/ProfileEntityConfiguration.cs
+++ BUSINESS/User.DataAccess/EntityConfiguration/ProfileEntityConfiguration.cs
@@ -1,6 +1,17 @@
-﻿namespace User.DataAccess.EntityConfiguration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using User.Core.Entities;
 
-public class ProfileEntityConfiguration
-{
-    
+namespace User.DataAccess.EntityConfiguration;
+
+public class ProfileEntityConfiguration : IEntityTypeConfiguration<Profile>
+{ 
+    public void Configure(EntityTypeBuilder<Profile> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.User).IsRequired();
+        builder.Property(p => p.Name).IsRequired();
+        builder.Property(p => p.LastName).IsRequired();
+        builder.ToTable("Profiles");
+    }
 }
--- BUSINESS/User.DataAccess/EntityConfiguration/UserEntityConfiguration.cs
+++ BUSINESS/User.DataAccess/EntityConfiguration/UserEntityConfiguration.cs
@@ -1,6 +1,20 @@
-﻿namespace User.DataAccess.EntityConfiguration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using User.Core.Entities;
 
-public class UserEntityConfiguration
+namespace User.DataAccess.EntityConfiguration;
+
+public class UserEntityConfiguration: IEntityTypeConfiguration<Core.Entities.User>
 {
-    
+    public void Configure(EntityTypeBuilder<Core.Entities.User> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.UserName).IsRequired();
+        builder.Property(s => s.Password).IsRequired();
+        builder.ToTable("Users");
+        
+        builder.HasOne(x => x.Profile)
+            .WithOne(x => x.User)
+            .HasForeignKey<Profile>(x => x.UserId);
+    }
 }
