a93c25d7fa166e730df1a89364bbe6023b72e76b
umutulupinar <umutulupinarrr@gmail.com>
2023-08-09 18:53:39 +0300

    Context class is added and coded

---
+++ BUSINESS/User.DataAccess/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using User.DataAccess.EntityConfiguration;
+
+namespace User.DataAccess;
+
+public class Context :DbContext
+{
+    public Context(DbContextOptions<Context> options):base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new ProfileEntityConfiguration());
+    }
+}
