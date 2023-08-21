10b84e60f595111e5b743c456f9c6ea133337356
umutulupinar <umutulupinarrr@gmail.com>
2023-08-21 19:03:53 +0300

    update profile action is added

---
+++ BUSINESS/User.API/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using User.Service.Models.Profile;
+using User.Service.Profile;
+
+namespace User.API.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class ProfileController : Controller
+{
+    private readonly IProfileService _profileService;
+
+    public ProfileController(IProfileService profileService)
+    {
+        _profileService = profileService;
+    }
+
+    public Task<ProfileUpdateResponseModel> UpdateProfile(ProfileUpdateRequestModel requestModel)
+    {
+        return _profileService.UpdateProfile(requestModel);
+    }
+}
